c16a179a0fa8fc4e2a02cbaab88de8582ff052c4
zakiafada32 <afadazaki32@gmail.com>
2021-04-15 21:23:34 +0700

    web socket connection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,10 +7,12 @@ import (
 	"github.com/zakiafada32/chat-cast/internal/handlers"
 )
 
+// routes defines the application routes
 func routes() http.Handler {
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
 
 	return mux
 }
